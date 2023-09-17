7af1145a131bfdb22788b66fdee2ff9f15d23fff
Hex <hexmusictheory@protonmail.com>
2023-09-17 01:58:07 +0100

    finish first two challenges

--- src/server.js
+++ src/server.js
@@ -2,4 +2,15 @@ const express = require("express");
 
 const server = express();
 
+server.get("/", (req, res) => {
+    res.send(`<h1>Hello Express</h1>`);
+});
+
+server.get("/colour", (req, res) => {
+    const hex = req.query.hex || "ffffff";
+    res.send(`
+    <style>body { background-color: #${hex}; }</style>
+    `);
+});
+
 module.exports = server;
